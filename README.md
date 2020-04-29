ded435d78e90f006973d523536998d08046b7f32
Clément Lombard <clement.lombard@wavestone.com>
2020-04-29 18:17:06 +0200

    Added support for categorical features

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pymindiff
+
+pymindiff is a Python 3 package that can be used to assign elements to a
+specified number of groups and minimize differences between created
+groups. 
+
+## Installation
+
+## Usage
--- pymindiff/groups.py
+++ pymindiff/groups.py
@@ -5,21 +5,46 @@ from pymindiff.scale import MinMaxScaler
 
 #TODO Address the problem of metrics weighting more in the sum than others
 #TODO Exact solution
-#TODO Handle categorical data
+#TODO Add the possibility to pass data that already had been optimized to optimize again and try to find better groups
 #TODO Write unit tests
+#TODO Write README
+#TODO Write docs
+#TODO Work on performance
+#TODO Don't rely on pandas ?
+#TODO integrate to Pypi
 
-def create_groups(data : pd.DataFrame, criteria : list, n_groups : int = 2, n_iter : int = 100, equalize=[np.mean], scale=False) -> pd.DataFrame:  
-    if any(column not in data.columns.values for column in criteria):
-        raise ValueError("Column not found in dataframe")
+def is_nominal_tolerance_met(data : pd.DataFrame, criteria_nominal : list = [], nominal_tolerance : list = []):
+    if len(criteria_nominal) == 0:
+        return True
+    else:
+        for column_criteria, tol in zip(criteria_nominal, nominal_tolerance):
+            nominal_check = data.groupby([column_criteria, 'subset']).size()
+            nominal_diff_values = [max(nominal_check[i].values) - min(nominal_check[i].values) for i in data[column_criteria].unique()]
+            if max(nominal_diff_values) > tol:
+                return False
+        return True
+
+
+def create_groups(data : pd.DataFrame, criteria : list = [], criteria_nominal : list = [], nominal_tolerance : list = [], n_groups : int = 2, n_iter : int = 100, equalize=[np.mean], scale=False) -> pd.DataFrame: 
+    assert len(criteria) > 0 or len(criteria_nominal) > 0, "No critera passed !"
+    assert len(criteria_nominal) == len(nominal_tolerance), "Not enough or too many tolerances, please pass as many tolerance values as there are nominal criterias to consider"
+    if any(column not in data.columns.values for column in criteria + criteria_nominal):
+        raise ValueError("At least one column was not found in the dataframe")
     else:
         diff_scores = list()
         if scale:
             scaler = MinMaxScaler()
             data[criteria] = scaler.fit_transform(data[criteria].copy())
         for i in range(n_iter):
+            #FIX so that groups are as equal in size as possible
             data['subset'] = np.random.choice([i for i in range(n_groups)], size=len(data))
-            try:
-                total_diff = 0
+            total_diff = 0
+            #Check that the tolerance is met for nominal criterias, if not go to next iteration
+            if is_nominal_tolerance_met(data, criteria_nominal, nominal_tolerance):
+                if len(criteria) == 0:
+                    #If there are only nominal criterias to minimize, return the first grouping below tolerance
+                    data['groups'] = data['subset']
+                    return data
                 for metric in equalize:
                     sum_of_columns_diff = 0
                     group_values = data.groupby(['subset'])[criteria].apply(metric)
@@ -29,15 +54,17 @@ def create_groups(data : pd.DataFrame, criteria : list, n_groups : int = 2, n_it
                     #We keep the biggest difference between 2 groups for each criteria as the treshold
                     total_diff += sum_of_columns_diff
                 diff_scores.append(total_diff)
-            except TypeError as e:
-                raise ValueError('The metric could not be computed using the column values, is the data numeric ?')
-            if len(diff_scores) == 1:
-                min_diff = diff_scores[0]
-                data['groups'] = data['subset']
-            else:
-                if min_diff > min(diff_scores):
-                    min_diff = min(diff_scores)
+                if len(diff_scores) == 1:
+                    min_diff = diff_scores[0]
                     data['groups'] = data['subset']
+                else:
+                    if min_diff > min(diff_scores):
+                        min_diff = min(diff_scores)
+                        data['groups'] = data['subset']
         if scale:
             data[criteria] = scaler.inverse_transform(data[criteria].copy())
+    if 'groups' not in data.columns.values:
+        print("No grouping found, probably because of a low tolerance on nominal criterias")
+        data = data.drop(columns=['subset'])
+    print(diff_scores)
     return data
